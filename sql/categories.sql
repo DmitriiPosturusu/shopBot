23d52814db9cb6455382d116987931144bbf5810
dima012111 <dima012111@gmail.com>
2024-09-05 13:59:27 +0300

    Add sql utilities

---
+++ sql/categories.sql
@@ -0,0 +1,12 @@
+INSERT INTO categories (category_id, category_name_en, category_name_ro,category_day_of_week) VALUES
+(1,'Menu of the Day','Meniu Zilei',False),
+(2,'Sandwich','Sandwich',False),
+(3,'Salads','Salate',False),
+(4,'Drinks','Bauturi',False),
+(101,'Monday','Luni',True),
+(102,'Tuesday','Marti',True),
+(103,'Wednesday','Miercuri',True),
+(104,'Thursday','Joi',True),
+(105,'Friday','Vineri',True),
+(106,'Saturday','Sambata',True),
+(107,'Sunday','Duminica',True);
